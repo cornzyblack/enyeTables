4619c35a303a69d1b30c5e7a4be84071fc79b1e1
Eniwoke <eniwokepdm@gmail.com>
2019-10-28 14:00:25 +0100

    add logic for reducer

--- redux-base.js
+++ redux-base.js
@@ -1,10 +1,34 @@
-// Store
+const redux = '';
+const createStore = redux.createStore;
 
+const initialState = {
+    formRows: [
+    ]
+}
 
 // Reducer
+const rootReducer = (state = initialState, action) => {
+    if (action.type === "ADD_FORM_TABLE") {
+        return {
+            formRows: [...state.formRows,
+            action.value]
+        }
+    }
+    return state;
+};
+
+// Store
+const store = createStore(rootReducer);
 
 
 // Dispatching Action
+// key: passedRow['key'],
+// firstName: passedRow['firstName'],
+// lastName: passedRow['lastName'],
+// age: passedRow['age'],
+// birthday: passedRow['birthday'].format('YYYY/MM/DD'),
+// hobby: passedRow['hobby'],
 
+store.dispatch({type: 'ADD_FORM_TABLE', payload: {} });
 
 // Subscription
